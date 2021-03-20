f9e06d84a4127b76f41f71957bb41b9ba226d1cf
WilsonBenavides <benavidesb@unicauca.edu.co>
2021-03-19 23:14:33 -0500

    working in spectrum slots view

--- node/DisplayManager.cc
+++ node/DisplayManager.cc
@@ -19,8 +19,9 @@ public:
 protected:
     virtual void initialize(int stage) override;
     virtual int numInitStages() const override; // NOTE THE const MODIFIER!!!
-
     virtual void handleMessage(cMessage *msg) override;
+    cCanvas *canvas;
+    void drawSlotGrid(int slotSize, int linkSize, cFigure::Color color);
 };
 
 Define_Module(DisplayManager);
@@ -41,34 +42,44 @@ DisplayManager::~DisplayManager()
 void DisplayManager::initialize(int stage)
 {
     if (stage == 2) {
+        canvas = getParentModule()->getParentModule()->getCanvas();
         EV << " Display Manager, stage 2 " << endl;
-        cCanvas *canvas = getParentModule()->getParentModule()->getCanvas();
 
-//        cLineFigure *line = new cLineFigure("line");
-//        line->setStart(cFigure::Point(0, 0));
-//        line->setEnd(cFigure::Point(100, 50));
-//        line->setLineWidth(2);
-//        line->setEndArrowhead(cFigure::ARROW_BARBED);
-//
-//        cArcFigure *arc = new cArcFigure("arc");
-//        arc->setBounds(cFigure::Rectangle(10,10,100,100));
-//        arc->setStartAngle(0);
-//        arc->setEndAngle(M_PI/2);
-//        arc->setLineColor(cFigure::BLUE);
-//        arc->setEndArrowhead(cFigure::ARROW_BARBED);
-
-        cRectangleFigure *rect = new cRectangleFigure("rect");
-        rect->setBounds(cFigure::Rectangle(100, 100, 160, 100));
-        rect->setCornerRadius(5);
-        rect->setFilled(true);
-        rect->setFillColor(cFigure::GOOD_LIGHT_COLORS[0]);
-
-        canvas->addFigure(rect);
-//        canvas->addFigure(arc);
+//      #2A7997,blue   #A0E0A0,transparent   0xD1D1D1,gray  0x0086C6,dark_blue
+        drawSlotGrid(10, 5, cFigure::Color("#A0E0A0"));
     }
 }
 
 void DisplayManager::handleMessage(cMessage *msg)
 {
+}
+
+void DisplayManager::drawSlotGrid(int slotSize, int linkSize, cFigure::Color color)
+{
+    cRectangleFigure *linkGrid[slotSize];
+
+    for (int lnk = 0; lnk < linkSize; lnk++) {
+        for (int slt = 0; slt < slotSize; slt++) {
+            int x = 800, y = 40;
+            cRectangleFigure *rect = new cRectangleFigure();
+            rect->setBounds(cFigure::Rectangle(x + (20 * slt), y + (20 * lnk + 10), 20, 20));
+            rect->setCornerRadius(1);
+            rect->setLineColor(cFigure::Color("#000000"));
+            rect->setLineWidth(2);
+
+            rect->setFilled(true);
+            rect->setFillColor(color);
+
+            if (lnk == 0 && (slt == 0 || slt == 1)) {
+                rect->setFillColor(cFigure::Color("#000000"));
+            }
+
+//            linkGrid[slt] = rect;
+            canvas->addFigure(rect);
+        }
+    }
+
+//    linkGrid->push_back(rect);
+
 }
 
