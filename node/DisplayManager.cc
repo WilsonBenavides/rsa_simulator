f21f164e3c52cc3a2a7a36931a9aa5dd54d7abfa
WilsonBenavides <benavidesb@unicauca.edu.co>
2021-03-20 01:43:15 -0500

    display spectrum grid compete, functions drawSlotGrid, drawSlotOnGrid implemented

--- node/DisplayManager.cc
+++ node/DisplayManager.cc
@@ -12,16 +12,19 @@ using namespace omnetpp;
  */
 class DisplayManager : public cSimpleModule
 {
+    cCanvas *canvas;
+    int slotSize = 20, linkSize = 21;
 public:
-    DisplayManager();
+    DisplayManager() = default;
     virtual ~DisplayManager();
 
 protected:
     virtual void initialize(int stage) override;
     virtual int numInitStages() const override; // NOTE THE const MODIFIER!!!
     virtual void handleMessage(cMessage *msg) override;
-    cCanvas *canvas;
     void drawSlotGrid(int slotSize, int linkSize, cFigure::Color color);
+    void drawSlotsOnGrid(int xpos, int ypos, int numSlots, cFigure::Color color);
+
 };
 
 Define_Module(DisplayManager);
@@ -31,22 +34,23 @@ int DisplayManager::numInitStages() const
     return 3;
 }
 
-DisplayManager::DisplayManager()
-{
-}
-
 DisplayManager::~DisplayManager()
 {
 }
 
 void DisplayManager::initialize(int stage)
 {
+    canvas = getParentModule()->getParentModule()->getCanvas();
+
     if (stage == 2) {
-        canvas = getParentModule()->getParentModule()->getCanvas();
         EV << " Display Manager, stage 2 " << endl;
 
-//      #2A7997,blue   #A0E0A0,transparent   0xD1D1D1,gray  0x0086C6,dark_blue
-        drawSlotGrid(10, 5, cFigure::Color("#A0E0A0"));
+//      #2A7997,blue   #A0E0A0,transparent   #D1D1D1,gray  #0086C6,dark_blue
+        drawSlotGrid(slotSize, linkSize, cFigure::Color("#A0E0A0"));
+        drawSlotsOnGrid(1, 0, 2, cFigure::Color("#21ab73"));
+        drawSlotsOnGrid(3, 0, 4, cFigure::Color("#5f1350"));
+        drawSlotsOnGrid(2, 1, 4, cFigure::Color("#cb5184"));
+        drawSlotsOnGrid(6, 1, 1, cFigure::Color("#7d4ea3"));
     }
 }
 
@@ -56,13 +60,11 @@ void DisplayManager::handleMessage(cMessage *msg)
 
 void DisplayManager::drawSlotGrid(int slotSize, int linkSize, cFigure::Color color)
 {
-    cRectangleFigure *linkGrid[slotSize];
-
     for (int lnk = 0; lnk < linkSize; lnk++) {
         for (int slt = 0; slt < slotSize; slt++) {
-            int x = 800, y = 40;
+            int x = 800, y = 20;
             cRectangleFigure *rect = new cRectangleFigure();
-            rect->setBounds(cFigure::Rectangle(x + (20 * slt), y + (20 * lnk + 10), 20, 20));
+            rect->setBounds(cFigure::Rectangle(x + (20 * slt), y + (25 * lnk), 20, 20));
             rect->setCornerRadius(1);
             rect->setLineColor(cFigure::Color("#000000"));
             rect->setLineWidth(2);
@@ -70,16 +72,17 @@ void DisplayManager::drawSlotGrid(int slotSize, int linkSize, cFigure::Color col
             rect->setFilled(true);
             rect->setFillColor(color);
 
-            if (lnk == 0 && (slt == 0 || slt == 1)) {
-                rect->setFillColor(cFigure::Color("#000000"));
-            }
-
-//            linkGrid[slt] = rect;
             canvas->addFigure(rect);
         }
     }
 
-//    linkGrid->push_back(rect);
-
 }
 
+void DisplayManager::drawSlotsOnGrid(int xpos, int ypos, int numSlots, cFigure::Color color)
+{
+    int pos = xpos + slotSize * (ypos) + 1;
+    for (int i = pos; i < pos + numSlots; i++) {
+        cRectangleFigure *rct = static_cast<cRectangleFigure*>(canvas->getFigure(i));
+        rct->setFillColor(color);
+    }
+}
